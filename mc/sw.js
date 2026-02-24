6c9a4eea68d837c744f2ca170a3bbd9e4d7c9189
darkdeals <93559186+darkdeals@users.noreply.github.com>
2026-02-24 00:14:23 -0800

    Adds PWA support for MC Playmat (#1)
    * Create manifest.json
    
    * Create index.html
    
    * Create sw.js

---
+++ mc/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'mc-playmat-v1';
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(['./', './manifest.json']);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
